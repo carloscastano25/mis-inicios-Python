32a6ffd1d02b13621e013822b6c839d3192b6608
carloscastano25 <carlos.castano@est.iudigital.edu.co>
2024-10-10 07:45:51 -0500

    Más ejercicios realizados

--- ejerciciosSemana1_3.py
+++ ejerciciosSemana1_3.py
@@ -37,22 +37,104 @@
 # print(f"El resultado de elevar {a} a la {b} es {a**b}")
 # print(f"El modulo de la division entre {a} y {b} es {a%b}")
 
-"""Condicionales:"""
+# """Condicionales:"""
 
-# Ejercicio 4: Clasificación de edad
+# # Ejercicio 4: Clasificación de edad
 
 
-try:
-    edad = int(input("Por favor ingrese su edad:  "))
-except ValueError:
-    print(f"El número debe ser un entero:")
+# try:
+#     edad = int(input("Por favor ingrese su edad:  "))
+# except ValueError:
+#     print(f"El número debe ser un entero:")
 
-if edad < 18:
-    print(f"Menor de edad")
-elif edad >= 18 and edad < 65:
-    print(f"Adulto")
-else:
-    print(f"Adulto mayor")
+# if edad < 18:
+#     print(f"Menor de edad")
+# elif edad >= 18 and edad <= 65:
+#     print(f"Adulto")
+# else:
+#     print(f"Adulto mayor")
+
+# # Ejercicio 5: Calculadora básica con condicionales
+
+# print(f"LISTA DE OPERADORES")
+
+# print(f"1. SUMA")
+# print(f"2. RESTA")
+# print(f"3. MULTIPLICACIÓN")
+# print(f"4. DIVISIÓN")
+
+# try:
+#     operador = int (input("Por favor indique el número correspondiente a la operación que desea realizar: "))
+# except ValueError:
+#     print(f"Debe ingresar un número entero de 1 a 4")
+
+# print(f"Ingrese dos números a operar:")
+# try:
+#     a = int(input("Primer número: "))
+#     b = int(input("Segundo número: "))
+# except ValueError:
+#     print(f"Los valor debe ser números enteros:")
+    
+# if operador == 1:
+#     print(f"La suma de los números ingresados es {a+b}")
+# elif operador == 2:
+#     print(f"La resta de los números ingresados es {a-b}")
+# elif operador == 3:
+#     print(f"La multiplicación de los números ingresados es {a*b}")
+# elif operador == 4:
+#     if b != 0:
+#         print(f"La división de los números ingresados es {a/b}")
+#     else:
+#         print(f"No es posible dividir entre cero")
+# else:
+#     print(f"Ese número no corresponde a ninguna operación en la lista")
+
+# #Ejercicio 6: Determinar si un año es bisiesto
+
+
+# anio = int(input("Por favor ingrese un año"))
+
+# if anio % 4 == 0:
+#     print(f"El año es bisiesto")
+# elif anio % 100 == 0 and anio % 400 != 0:
+#         print(f"El año no es bisiesto")
+# else:
+#     print(f"El año no es bisiesto")
+
+# """Bucles:"""
+
+# # Ejercicio 7: Suma de dígitos
+
+
+# numero = int(input("Por favor ingrese un número entero positivo: "))
+
+# while numero < 0:
+#     numero = int(input("Por favor ingrese un entero positivo: "))
+    
+# suma_digitos = 0
+
+# while numero > 0:
+#     digito = numero % 10 #! Con esta funcionalidad en digito guardo el último dígito del número
+#     suma_digitos += digito
+#     numero //= 10 # !Con esta funcionalidad // se divide con resultado entero, osea que se elimina el último digito
+
+# print(f"La suma de los dígitos es {suma_digitos}")
+
+# #Ejercicio 8: Encontrar el número más pequeño en una lista
+
+# lista = [8,2,3,4,5,6]
+
+# menor_numero = lista[0]
+
+# for numero in lista:
+#     if numero < menor_numero:
+#         menor_numero = numero
+
+# print(f"El número más pequeño es {menor_numero}")
+    
+
+
+
 
 
 
