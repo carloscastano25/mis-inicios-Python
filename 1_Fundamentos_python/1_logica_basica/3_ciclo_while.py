71712b8cf20c4c94c4aa900d09feb6b47a061198
carloscastano25 <carlos.castano@est.iudigital.edu.co>
2025-01-12 02:03:46 -0500

    Trabajando en diccionarios

--- 1_Fundamentos_python/1_logica_basica/3_ciclo_while.py
+++ 1_Fundamentos_python/1_logica_basica/3_ciclo_while.py
@@ -12,6 +12,11 @@
 #? sea verdadera. Se evalúa la condición antes de cada iteración, y el ciclo se detiene cuando 
 #? la condición se vuelve falsa.
 
+#! IMPORTANTE: 
+#? No solo cuando la condición se vuelve falsa es que se rompe el ciclo, también cuando añado un break, es decir
+#? cuando le doy una condición de rompimiento al ciclo, also como, si la condición no se ha cumplido, pero 
+#? pasa tal cosa, rómpete y termina el ciclo. 
+
 #! SINTAXIS!!!
 
 # while condicion:
@@ -19,26 +24,33 @@
     
 #TODO: EJEMPLO BÁSICO:
 
-contador = 1          #?Creo una variable llamada contador y la inicializo en 1
-while contador <= 5:  #? Establezco la condición de que mientras el contador sea menor o igual a 5
-    print(contador)   #? Muéstrame el contador
-    contador += 1     #? Incrementar el contador en cada iteración de uno en uno para que pueda llegar a ser 5
-#                     #?-- y así llegar al fin del ciclo, si no lo hago, el ciclo sería infinito. 
+# contador = 1          #?Creo una variable llamada contador y la inicializo en 1
+# while contador <= 5:  #? Establezco la condición de que mientras el contador sea menor o igual a 5
+#     print(contador)   #? Muéstrame el contador
+#     contador += 1     #? Incrementar el contador en cada iteración de uno en uno para que pueda llegar a ser 5
+#                       #?-- y así llegar al fin del ciclo, si no lo hago, el ciclo sería infinito. 
 
-# counter = 0   
-# while counter <11:
-#     if counter % 2 != 0:
-#         print(f"{counter}")
-#     counter += 1
+#TODO: EJEMPLO CON UN IF:
+# counter = 0                   #? Creo una variable counter y la inicializo en 0
+# while counter <11:            #? Doy una condición de que se haga lo de adentro mientras el contador sea menor a 11
+#     if counter % 2 != 0:      #? Creo una condición interna, si el counter no es par
+#         print(f"{counter}")   #? Muéstralo en la pantalla (De modo que no se muestran los números pares)
+#     counter += 1              #? Incrementar el contador en cada iteración de uno en uno para que pueda llegar a ser 5
+#                               #?-- y así llegar al fin del ciclo, si no lo hago, el ciclo sería infinito.
 
+#TODO USO DE ELSE CON WHILE
+                        #? Cuando un ciclo finaliza, tengo la opción de poner un mensaje de salida, indicando
+                        #? que el ciclo ha culminado porque ya no se cumple la condición, como a continuación:
+                        
+n = 3                   #? Creo una variable y le asigno el valor de 3
 
-# n = 3
+while n > 0:            #? Mientras se cupla la condición de que n sea mayor a 0 
+    print(n + 1)        #? Si eso se cumple, realiza esto de mostrar n + 1, es decir, haz la suma y muestra el resultado
+    n -= 1              #? Ahora a n se le asignará lo que hay en n menos 1, para que el ciclo se rompa en algún momento
+else:                   #!Esto es lo diferente ahora, si detallamos, el ciclo while es casi un if pero en ciclos, por eso, cuando ya no se cumple la fucondición, puedo poner un else
+    print("Ya no se cumple la función, ya se llegó a ", n)  #! En ese else, puedo poner un mensaje informando que el ciclo ya se rompió porque no se cumple la condición
 
-# while n > 0:
-#     print(n + 1)
-#     n -= 1
-# else:
-#     print(n)
+#TODO: Otro ejemplo de while usando else cuando el ciclo finaliza
 
 # i = 1
 # while i < 5:
--- 1_Fundamentos_python/2_iterables/3_diccionarios.py
+++ 1_Fundamentos_python/2_iterables/3_diccionarios.py
@@ -6,3 +6,69 @@
 #!             | |_| ||  || |____ | |____ |  || |_| || |\   | / /    \ \ | | \ \ |  || |_| | ___| | 
 #!             |____/ |__| \_____| \_____||__| \___/ |_| \__|/_/      \_\|_|  \_\|__| \___/ |_____| 
 
+#TODO: DEFINICIÓN:
+
+#? Un diccionario es una estructura de datos en Python que almacena pares de clave: valor. 
+#? Cada clave es única y se utiliza para acceder a su valor asociado. Los diccionarios son mutables, 
+#? lo que significa que sus contenidos pueden cambiarse después de su creación.
+
+
+#TODO: SINTAXIS: 
+#! Los diccionarios se definen con llaves
+
+# diccionario = {} #? Puedo crear un diccionario vació para llenar luego
+
+diccionario = {               #? Primero se define el nombre del diccionario, que será igual a lo que hay entre
+    "clave1": "valor1",       #? las llaves, se puede ver que el diccionario al igual que un diccionario común
+    "clave2": "valor2",       #? consta de una palabra clave y su valor correspondiente, como un diccionario
+    "clave3": "valor3"        #? Ingles - Español. La clave a la izquierda, el valor a la derecha después de :
+}
+
+#! LAS CLAVES DE UN DICCIONARIO SON ÚNICAS E INMUTABLES:
+#? Las claves deben ser String, enteros, números flotantes o tuplas.
+
+#! PERO LOS VALORES SÍ PUEDEN CAMBIAR Y TENER CUALQUIER VALOR QUE QUIERA ASIGNARLES:
+#? Los valores sí pueden ser String, números, funciones, tuplas, listas, flotantes y cualquier otro tipo de dato
+
+#TODO: Ejemplo de un diccionario simple:
+
+# mi_diccionario = {
+#     "nombre": "Juan",
+#     "edad": 25,
+#     "ciudad": "Madrid"
+# }
+
+
+#                         #TODO: PARA ACCEDER A LOS VALORES DE UNA CLAVE
+# mi_diccionario["nombre"]      #? Para acceder al valor correspondiente a esa clave ---> Juan
+# mi_diccionario.get("nombre")  #? Arroja el mismo resultado, acceso al valor de la clave ---> Juan
+# print(mi_diccionario.get("universidad", "Dato no guardado"))  #!OJO CON ESTO: 
+                                                            #? Ojo aquí, mi diccionario, en realidad, no tiene
+                                                            #? una clave llamada universidad, por lo que puedo
+                                                            #? darle un valor por defecto, pero, ese valor no queda
+                                                            #? guardado, solo se mostrará en esa consulta.
+
+#                                   #TODO: PARA MODIFICAR UN DICCIONARIO
+# mi_diccionario["edad"] = 26               #? Cambia el valor correspondiente a la clave edad, pasa de 25 a 26
+# mi_diccionario["profesión"] = "Ingeniero" #? Agrega una nueva clave con su respectivo valor a mi diccionario
+# del mi_diccionario["ciudad"]              #? Esto borra la clave y su valor correspondiente. 
+
+#                                #TODO: MÉTODOS A USAR CON LOS DICCIONARIOS
+
+#        #! .get(clave)     .get(clave, valor_por_defecto)
+#? Este se usa para adquirir el valor correspondiente a una clave que es la que se pone entre los paréntesis
+#? Si no existe el resultado es None, pero puedo ponerle un resultado por defecto si no existe ticha clave
+#? En el ejemplo, c no existe como clave, pero para que no me diga None, le pongo un valor por defecto
+
+#TODO: Ejemplo práctico:
+
+diccionario = {
+    "a": 1,
+    "b": 2
+    }
+
+print(diccionario.get("a"))  #? Output: 1
+print(diccionario.get("b"))  #? Output: 2
+print(diccionario.get("c"))  #? Output: None
+print(diccionario.get("d", "Clave no encontrada"))  #? Output: Clave no encontrada
+
